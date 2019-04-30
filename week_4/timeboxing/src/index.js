7f14afc9ebd4305665befe47750ae4be297a9fa4
basiakus <basiakus@gmail.com>
2019-04-30 21:02:41 +0200

    cleaning

--- week_4/timeboxing/src/index.js
+++ week_4/timeboxing/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/App';
+import './styles/style.css';
 /* Elements from DOM */
 const rootElement = document.getElementById("root");
 
