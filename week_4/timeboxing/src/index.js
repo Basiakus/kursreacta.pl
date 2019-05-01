f198f19f070d4aa2c1a46d1cd47a028431d04582
basiakus <basiakus@gmail.com>
2019-05-01 12:54:03 +0200

    add style path

--- week_4/timeboxing/src/index.js
+++ week_4/timeboxing/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './styles/style.css';
+import './styles/main.scss';
 /* Elements from DOM */
 const rootElement = document.getElementById("root");
 
