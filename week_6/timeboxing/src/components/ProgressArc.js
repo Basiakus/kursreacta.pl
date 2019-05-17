5a5b5cf230e30764cc96656bc76c706123067b0b
basiakus <basiakus@gmail.com>
2019-05-17 19:32:52 +0200

    create component

---
+++ week_6/timeboxing/src/components/ProgressArc.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ProgressArc extends Component {
+	constructor(props) {
+		super(props);
+		this.canvas = 
+	}
+
+	render() {
+		return (
+			<div>
+				ARC
+			</div>
+		)
+	}
+}
+
+export default ProgressArc;
