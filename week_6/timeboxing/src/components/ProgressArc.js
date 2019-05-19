7f3b35730808767653704ac14dc96c11799967f7
basiakus <basiakus@gmail.com>
2019-05-19 17:51:49 +0200

    change size on the page

--- week_6/timeboxing/src/components/ProgressArc.js
+++ week_6/timeboxing/src/components/ProgressArc.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import '../styles/components/ProgressArc.scss';
 class ProgressArc extends Component {
 	constructor(props) {
 		super(props);
@@ -21,10 +21,10 @@ class ProgressArc extends Component {
 		const ctx = this.canvas.current.getContext("2d");
 		const { canvasSize, percent } = this.props;
 		ctx.clearRect(0, 0, canvasSize, canvasSize); //czyszczenie obszaru
-		ctx.fillStyle = 'rgb(299, 100, 0, .3)'; //color zawartości 
+		ctx.fillStyle = 'rgb(299, 100, 0, 0)'; //color zawartości 
 		ctx.fillRect(0, 0, canvasSize, canvasSize); //obszar zawartości
 
-		const gradient = ctx.createLinearGradient(180, 60, 30, 0);
+		const gradient = ctx.createLinearGradient(100, 150, 200, 0);
 		gradient.addColorStop("0", `rgb(255, ${this.colorValue}, 0)`);//red
 		gradient.addColorStop(".5", `rgb(0, 255, ${this.colorValue})`);//green
 		gradient.addColorStop("1", `rgb(${this.colorValue}, 0, 255)`);//blue
@@ -33,8 +33,8 @@ class ProgressArc extends Component {
 		//console.log(ringPercent);
 
 		ctx.beginPath();
-		ctx.lineWidth = canvasSize / 20;
-		ctx.lineCap = "round";
+		ctx.lineWidth = canvasSize / 5 ;
+		//ctx.lineCap = "round";
 		let ringPercent = percent < 0 ? 0 : (2 / 100) * percent;
 		const x = canvasSize / 2;
 		const y = x;
@@ -48,12 +48,12 @@ class ProgressArc extends Component {
 		this.colorValue = !this.isOver ? this.colorValue += 2 : this.colorValue -= 2;
 		this.animationRequest = window.requestAnimationFrame(this.updateColor);
 		this.drawCircleCanvas();
-		console.log(this.colorValue, this.isOver);
+		//console.log(this.colorValue, this.isOver);
 	}
 	render() {
 		const { canvasSize } = this.props;
 		return (
-			<canvas ref={this.canvas} width={canvasSize} height={canvasSize} />
+			<div className='ProgressArc'><canvas ref={this.canvas} width={canvasSize} height={canvasSize} /></div>
 		)
 	}
 }
