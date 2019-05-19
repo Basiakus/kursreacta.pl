be43929618cdf5d0b29c61afc6ece32aef1ba483
basiakus <basiakus@gmail.com>
2019-05-19 14:17:51 +0200

    add requestAnimationFrame

--- week_6/timeboxing/src/components/ProgressArc.js
+++ week_6/timeboxing/src/components/ProgressArc.js
@@ -4,22 +4,35 @@ class ProgressArc extends Component {
 	constructor(props) {
 		super(props);
 		this.canvas = React.createRef();
+		this.colorValue = 200;
+		this.isOver = false;
 	}
 	componentDidMount() {
 		this.drawCircleCanvas();
+		this.animationRequest = window.requestAnimationFrame(this.updateColor);
 	}
 	componentDidUpdate() {
 		this.drawCircleCanvas();
+	}
+	componentWillUnmount() {
+		window.cancelAnimationFrame(this.animationRequest);
 	}
 	drawCircleCanvas = () => {
 		const ctx = this.canvas.current.getContext("2d");
 		const { canvasSize, percent } = this.props;
 		ctx.clearRect(0, 0, canvasSize, canvasSize); //czyszczenie obszaru
-		ctx.fillStyle = 'rgb(299, 100, 0, .3)'; //color zawartości 1
+		ctx.fillStyle = 'rgb(299, 100, 0, .3)'; //color zawartości 
 		ctx.fillRect(0, 0, canvasSize, canvasSize); //obszar zawartości
-		ctx.save();
+
+		const gradient = ctx.createLinearGradient(180, 60, 30, 0);
+		gradient.addColorStop("0", `rgb(255, ${this.colorValue}, 0)`);//red
+		gradient.addColorStop(".5", `rgb(0, 255, ${this.colorValue})`);//green
+		gradient.addColorStop("1", `rgb(${this.colorValue}, 0, 255)`);//blue
+		ctx.strokeStyle = gradient;
+		ctx.stroke();
+		//console.log(ringPercent);
+
 		ctx.beginPath();
-		ctx.fillStyle = 'rgb(100, 0, 00)';
 		ctx.lineWidth = canvasSize / 20;
 		ctx.lineCap = "round";
 		let ringPercent = percent < 0 ? 0 : (2 / 100) * percent;
@@ -27,8 +40,15 @@ class ProgressArc extends Component {
 		const y = x;
 		const radius = canvasSize / 2.5;
 		ctx.arc(x, y, radius, 0, ringPercent * Math.PI);
-		ctx.stroke();
-		console.log(ringPercent);
+	}
+	updateColor = () => {
+		if (this.colorValue > 254 || this.colorValue == 0) {
+			this.isOver = !this.isOver
+		}
+		this.colorValue = !this.isOver ? this.colorValue += 2 : this.colorValue -= 2;
+		this.animationRequest = window.requestAnimationFrame(this.updateColor);
+		this.drawCircleCanvas();
+		console.log(this.colorValue, this.isOver);
 	}
 	render() {
 		const { canvasSize } = this.props;
