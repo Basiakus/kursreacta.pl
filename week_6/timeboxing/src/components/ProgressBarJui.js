0c66b2c67eb9a23c451486c40058a0f606e692ef
basiakus <basiakus@gmail.com>
2019-05-20 20:28:41 +0200

    import progressBar from JQuery Library

---
+++ week_6/timeboxing/src/components/ProgressBarJui.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class ProgressBarJui extends Component {
+     constructor(props) {
+          super(props);
+          this.progressBasjui = React.createRef();
+          this.state = {
+
+          }
+     }
+     componentDidMount() {
+          window.$(this.progressBasjui.current).progressbar({
+               value: this.props.percent
+          })
+     }
+     componentDidUpdate() {
+          const value = window.$(this.progressBasjui.current).progressbar('option', 'value');
+          window.$(this.progressBasjui.current).progressbar('option', 'value', this.props.percent);
+     }
+     componentWillUnmount() {
+          window.$(this.progressBasjui.current).progressbar('destroy');
+     }
+     render() {
+          return (
+               <>
+                    <p>ProgressBar from JQuery</p>
+                    <div ref={this.progressBasjui} />
+               </>
+          )
+     }
+}
+
+export default ProgressBarJui;
