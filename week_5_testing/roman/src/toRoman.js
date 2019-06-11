c1df65e23e5d7bc61bc4d2467f61efba25e9eab6
basiakus <basiakus@gmail.com>
2019-06-11 21:11:17 +0200

    find copy paste

---
+++ week_5_testing/roman/src/toRoman.js
@@ -0,0 +1,21 @@
+// skopiowałem z https://blog.usejournal.com/create-a-roman-numerals-converter-in-javascript-a82fda6b7a60
+const toRoman = (number) => {
+     let roman = "";
+     const romanNumList = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XV: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+     let a;
+     if (number < 1 || number > 3999)
+          return "Enter a number between 1 and 3999";
+     else {
+          for (let key in romanNumList) {
+               a = Math.floor(number / romanNumList[key]);
+               if (a >= 0) {
+                    for (let i = 0; i < a; i++) {
+                         roman += key;
+                    }
+               }
+               number = number % romanNumList[key];
+          }
+     }
+     return roman;
+}
+export default toRoman;
