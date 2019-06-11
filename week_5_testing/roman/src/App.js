e91a3d39750fe7ab4a2d3a4baeac037f3e6a0a98
basiakus <basiakus@gmail.com>
2019-06-11 21:10:09 +0200

    checkimg test

--- week_5_testing/roman/src/App.js
+++ week_5_testing/roman/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import RomanConverter from './RomanConverter';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <RomanConverter />
     </div>
   );
 }
