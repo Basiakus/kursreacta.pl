f92584f6df54dd0cab39da9d04bd1f62cb4a2aff
basiakus <basiakus@gmail.com>
2019-06-13 23:37:59 +0200

    add ArabicConverter component

--- week_5_testing/roman/src/App.js
+++ week_5_testing/roman/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import RomanConverter from './RomanConverter';
-
+import ArabicConverter from './ArabicConverter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <RomanConverter />
+      <ArabicConverter />
     </div>
   );
 }
