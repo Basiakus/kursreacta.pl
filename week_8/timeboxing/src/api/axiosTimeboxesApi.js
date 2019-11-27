7240f6b02a9b64fec3e520df9fc6b399b1bbaaaa
basiakus <basiakus@gmail.com>
2019-11-27 18:38:12 +0100

    import(axios) locality to evey function

--- week_8/timeboxing/src/api/axiosTimeboxesApi.js
+++ week_8/timeboxing/src/api/axiosTimeboxesApi.js
@@ -1,13 +1,15 @@
-import axios from 'axios';
+//import axios from 'axios';
 
 const axiosTimeboxesApi = (baseURL = 'http://localhost:4000/timeboxes/') => {
      const timeboxesApi = {
           getAllTimeboxes: async function (accessToken) {
+               const axios = await import('axios');
                const response = await axios.get(baseURL, { headers: { Authorization: `Bearer ${accessToken}`} });
                const timeboxes = response.data;
                return timeboxes;
           },
-          getTimeboxesByFullTextSearch: async function (string, accessToken) {
+           getTimeboxesByFullTextSearch: async function (string, accessToken) {
+               const axios = await import('axios');
                const newBaseUrl = `${baseURL}?q=${string}`;
                const response = await axios.get(newBaseUrl, { headers: { Authorization: `Bearer ${accessToken}` } });
                const timeboxes = response.data;
@@ -15,11 +17,13 @@ const axiosTimeboxesApi = (baseURL = 'http://localhost:4000/timeboxes/') => {
                return timeboxes;
           },
           addTimebox: async function (timeboxToAdd, accessToken) {
+               const axios = await import('axios');
                const response = await axios.post(baseURL, timeboxToAdd, { headers: { Authorization: `Bearer ${accessToken}` } });
                const addedTimebox = response.data;
                return addedTimebox;
           },
           replaceTimebox: async function (timeboxToReplace, accessToken) {
+               const axios = await import('axios');
                if (!timeboxToReplace.id) {
                     throw new Error("id dont exist");
                }
@@ -28,6 +32,7 @@ const axiosTimeboxesApi = (baseURL = 'http://localhost:4000/timeboxes/') => {
                return replaceTimebox;
           },
           removeTimebox: async function (timeboxToRemove, accessToken) {
+               const axios = await import('axios');
                if (!timeboxToRemove.id) {
                     throw new Error("id dont exist");
                }
@@ -36,6 +41,7 @@ const axiosTimeboxesApi = (baseURL = 'http://localhost:4000/timeboxes/') => {
                return removeTimebox;
           },
           partiallyUpdateTimebox: async function (timeboxToUpdate, accessToken) {
+               const axios = await import('axios');
                if (!timeboxToUpdate.id) {
                     throw new Error("id dont exist");
                }
@@ -43,7 +49,7 @@ const axiosTimeboxesApi = (baseURL = 'http://localhost:4000/timeboxes/') => {
                const updateTimebox = response.data;
                console.log('axios patch running');
                return updateTimebox;
-          }
+          } 
      }
      return timeboxesApi;
 }
