e898e9d82958ee87c3c0bac5110159d2c79df85e
basiakus <basiakus@gmail.com>
2020-02-11 21:31:00 +0100

    set  InspiracionalQuote to funcional commponent

--- week_8/timeboxing/src/components/InspiracionalQuote.js
+++ week_8/timeboxing/src/components/InspiracionalQuote.js
@@ -1,38 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../styles/components/InspiracionalQuote.scss';
 
-class InspiracionalQuote extends React.Component  {
-     state = {
-          quote: null
-     }
+function InspiracionalQuote()  {
 
-     componentDidMount() {
+     const [quote, setQuote] =  useState(null);
+
+     useEffect(() => {
           import('inspirational-quotes').then((quote) => {
-               //console.log(quote.getQuote());
-               this.setState({
-                    quote: quote.getQuote()
-               })
+               setQuote(quote.getQuote());
           }).catch((error) => console.log(`sorry but from some reason can't get quote. Error ${error}`));
-     }
-     componentWillUnmount() {
-          this.setState({
-               quote: null
-          })
-     }
+     }) 
+          
      
-     render() {
-          return (
-               <>
-                    { this.state.quote !== null ?
-                         <figure className='InspiracionalQuote'>
-                              <blockquote>{this.state.quote.text}</blockquote>
-                              <figcaption className="InspiracionalQuote__author"><cite>{this.state.quote.author}</cite></figcaption>
-                         </figure> :
-                         '. . . l o a d i n g'
-                    }
-               </>
-          )
-     }
+     return (
+          <>
+               { quote !== null ?
+                    <figure className='InspiracionalQuote'>
+                         <blockquote>{quote.text}</blockquote>
+                         <figcaption className="InspiracionalQuote__author"><cite>{quote.author}</cite></figcaption>
+                    </figure> :
+                    '. . . l o a d i n g'
+               }
+          </>
+     )
 }
 
 export default InspiracionalQuote;
