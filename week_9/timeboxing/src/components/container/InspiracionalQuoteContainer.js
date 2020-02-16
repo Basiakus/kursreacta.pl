d653637174117124008cf9e5cb8e6fe93d4e7818
basiakus <basiakus@gmail.com>
2020-02-16 20:52:02 +0100

    create presentation component for InspiracionalQuotes

--- week_9/timeboxing/src/components/container/InspiracionalQuoteContainer.js
+++ week_9/timeboxing/src/components/container/InspiracionalQuoteContainer.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import InspiracionalQuote from '../InspiracionalQuote';
+import InspiracionalQuote from '../presentation/InspiracionalQuote';
 
 export default function InspiracionalQuoteContainer()  {
 
--- week_9/timeboxing/src/components/presentation/InspiracionalQuote.js
+++ week_9/timeboxing/src/components/presentation/InspiracionalQuote.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/components/InspiracionalQuote.scss';
+import '../../styles/components/InspiracionalQuote.scss';
 
 function InspiracionalQuote({ quote }) {
      return (
