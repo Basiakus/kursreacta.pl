261c8251634568006fadd102ab256eaa93d5077b
basiakus <basiakus@gmail.com>
2020-04-11 22:31:08 +0200

    get Reducer to CurentTimebox component

--- week_11/timeboxing/src/components/CurrentTimebox.js
+++ week_11/timeboxing/src/components/CurrentTimebox.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useReducer } from 'react';
 import Clock from './Clock';
 import ProgressBar from './ProgressBar';
 //import ProgressArc from './ProgressArc';
@@ -7,32 +7,71 @@ import { getMinutesAndSecondsFromDuractionInSeconds } from '../lib/time.js';
 
 import '../styles/components/CurrentTimebox.scss';
 
+const curentTimeboxReducer = (state, action) => {
+   switch (action.type) {
+      case "CURRENT_TIMEBOX_START": {
+         return { ...state, isRunning: true, isPaused: false };
+      }
+      case "CURRENT_TIMEBOX_STOP": {
+         return { ...state, isRunning: false };
+      }
+      case "CURRENT_TIMEBOX_STOP_HANDLER": {
+         return { ...state, isRunning: false, isPaused: false, pausesCount: 0, elapsedTimeInSeconds: 0};
+      }
+      case "CURRENT_TIMEBOX_PAUSE": {
+         const { currentPause } = action;
+         return { ...state, isPaused: currentPause};
+      }
+      case "CURRENT_TIMEBOX_PAUSES_COUNT": {
+         const { currentPause } = action;
+         let currentPausesValue = state.pausesCount;
+         if (currentPause) {
+            const pausesCount = currentPausesValue + 1;
+            return { ...state, pausesCount }
+         } else {
+            return null
+         }
+      }
+      case "CURRENT_TIMEBOX_ELAPSED_TIME": {
+         let { elapsedTimeInSeconds } = action;
+         elapsedTimeInSeconds = elapsedTimeInSeconds + 0.01;
+         return { ...state, elapsedTimeInSeconds };
+      }
+      case "CURRENT_TIMEBOX_ELAPSED_TIME_RESET": {
+         return { ...state, elapsedTimeInSeconds: 0 };
+      }
+      default: {
+         return state;
+      }
+   }
+}
+
 function CurrentTimebox({title, totalTimeInMinutes}) {
 
-   const [isRunning, setIsRunning] = useState(false);
+   /* const [isRunning, setIsRunning] = useState(false);
    const [isPaused, setIsPaused] = useState(false);
    const [elapsedTimeInSeconds, setElapsedTimeInSeconds] = useState(0);
-   const [pausesCount, setPausesCount] = useState(0);
-   let intervalId = useRef();
+   const [pausesCount, setPausesCount] = useState(0); */
 
-   const stateTwo = {
-      running: false,
-      paused: false,
-      elapsedSecond: 0,
-      pauses: 0
+   const inicialState = {
+      isRunning: false,
+      isPaused: false,
+      elapsedTimeInSeconds: 0,
+      pausesCount: 0
    }
 
+   let intervalId = useRef();
+   const [state, dispatch] = useReducer(curentTimeboxReducer, inicialState);
 
    const startTimer = () => {
-      setIsRunning(true);
-      setIsPaused(false)
-      console.log(isPaused)
+      dispatch({type: "CURRENT_TIMEBOX_START"})
+      console.log(state)
    };
 
    const stopTimer = (e) => {
-      setIsRunning(false)
+      dispatch({ type: "CURRENT_TIMEBOX_STOP" })
       window.clearInterval(intervalId.current);
-      console.log('timer stop')
+      console.log(state)
       
    };
    const handleStart = (e) => {
@@ -40,43 +79,37 @@ function CurrentTimebox({title, totalTimeInMinutes}) {
    };
    
    const handleStop = () => {
-      setIsRunning(false);
-      setIsPaused(false);
-      setPausesCount(0);
-      setElapsedTimeInSeconds(0);
+      dispatch({ type: "CURRENT_TIMEBOX_STOP_HANDLER" })
       stopTimer();
    };
    const togglePause = () => {
-      const currentPause = !isPaused;
-      setPausesCount(prev => {
-         let prevValue;
-         currentPause ? prevValue = prev + 1 : prevValue = prev;
-         return prevValue;
-      })
+      const currentPause = !state.isPaused;
+      dispatch({ type: "CURRENT_TIMEBOX_PAUSES_COUNT", currentPause })
       currentPause ? stopTimer() : startTimer();
-      setIsPaused(currentPause);
+      dispatch({ type: "CURRENT_TIMEBOX_PAUSE", currentPause })
    };
 
    useEffect(() => {
       let totalTimeInSeconds = totalTimeInMinutes * 60;
-      if (isRunning) {
+      if (state.isRunning) {
          intervalId.current = window.setInterval(() => {
-            setElapsedTimeInSeconds(prevValue => prevValue + 0.01)
+            const { elapsedTimeInSeconds } = state;
+            dispatch({ type: 'CURRENT_TIMEBOX_ELAPSED_TIME', elapsedTimeInSeconds })
             //console.log(elapsedTimeInSeconds, 'interval running')
             if (totalTimeInSeconds < elapsedTimeInSeconds) {
                stopTimer();
-               setElapsedTimeInSeconds(0)
+               dispatch({ type: 'CURRENT_TIMEBOX_ELAPSED_TIME_RESET' })
             }
          }, 10);
       }
       return () => window.clearInterval(intervalId.current);
-   }, [elapsedTimeInSeconds, isRunning, totalTimeInMinutes])
+   }, [state.elapsedTimeInSeconds, state.isRunning, totalTimeInMinutes])
    useEffect(() => handleStop(), [])
 
    const totalTimeInSeconds = totalTimeInMinutes * 60;
-   const timeLeftInSeconds = totalTimeInSeconds - elapsedTimeInSeconds;
+   const timeLeftInSeconds = totalTimeInSeconds - state.elapsedTimeInSeconds;
    const hoursLeft = totalTimeInSeconds >= 3600 ? Math.floor(timeLeftInSeconds / 3600) : 0;
-   const milisecondsLeft = (totalTimeInSeconds > elapsedTimeInSeconds) ? (timeLeftInSeconds - Math.floor(timeLeftInSeconds)).toFixed(2) : 0;
+   const milisecondsLeft = (totalTimeInSeconds > state.elapsedTimeInSeconds) ? (timeLeftInSeconds - Math.floor(timeLeftInSeconds)).toFixed(2) : 0;
    const decimalConverter = ((value) => (value % 1) * 1000);
    const progressInPercent = (timeLeftInSeconds / totalTimeInSeconds) * 100;
    const [minutesLeft, secondsLeft] = getMinutesAndSecondsFromDuractionInSeconds(hoursLeft, timeLeftInSeconds);
@@ -87,7 +120,7 @@ function CurrentTimebox({title, totalTimeInMinutes}) {
          pozostało: 
          {/* <ProgressArc canvasSize={80} percent={progressInPercent} /> */}
          <Clock
-            className={isPaused ? 'inactive' : ''}
+            className={state.isPaused ? 'inactive' : ''}
             hours={hoursLeft}
             hoursColor="black"
             minutes={minutesLeft}
@@ -99,7 +132,7 @@ function CurrentTimebox({title, totalTimeInMinutes}) {
             separatorColor="red"
          />
          <ProgressBar
-            className={isPaused ? 'inactive' : ''}
+            className={state.isPaused ? 'inactive' : ''}
             borderBlue={true}
             barColor="blue"
             isBig={true} 
@@ -114,10 +147,10 @@ function CurrentTimebox({title, totalTimeInMinutes}) {
             percent={progressInPercent}
             trackRemaining={false}
          /> */}
-         <button onClick={handleStart} disabled={isRunning}>Start</button>
-         <button onClick={handleStop} disabled={!isRunning}>Stop</button>
-         <button onClick={togglePause} >{isPaused ? 'Wznów' : 'Pauzuj'}</button>
-         liczba przerw: {pausesCount}
+         <button onClick={handleStart} disabled={state.isRunning}>Start</button>
+         <button onClick={handleStop} disabled={!state.isRunning}>Stop</button>
+         <button onClick={togglePause} >{state.isPaused ? 'Wznów' : 'Pauzuj'}</button>
+         liczba przerw: {state.pausesCount}
       </div>
    )
 }
