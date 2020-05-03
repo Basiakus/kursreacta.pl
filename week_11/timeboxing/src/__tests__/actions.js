9aa23e4089dddae2892ab9ee32093c84e1a70492
basiakus <basiakus@gmail.com>
2020-05-03 19:49:18 +0200

    naive test of redux

---
+++ week_11/timeboxing/src/__tests__/actions.js
@@ -0,0 +1,40 @@
+import {timeboxesReducer} from '../reducers/timeboxesReducer.js';
+import { updateTimebox } from '../actions/timeboxListMenagerActions.js';
+describe('timeboxesReducer', () => {
+     const state = {
+          timeboxes: [
+               {
+                    id: 'timebox to update',
+                    text: 'string to update'
+               }
+          ]
+     }
+     const updatedTimebox = {
+          id: 'timebox to update',
+          text: 'updated string'
+     }
+     test("upbate Timebox", () => {
+          expect(timeboxesReducer(state, { type: "TIMEBOX_UPDATE", updatedTimebox })).
+               toEqual({
+                    timeboxes: [
+                         {
+                              id: 'timebox to update',
+                              text: 'updated string'
+                         }
+                    ]
+               }
+          );
+     });
+     test('update timebox with inported action', () => {
+          expect(timeboxesReducer(state, updateTimebox(updatedTimebox))).
+               toEqual({
+                    timeboxes: [
+                         {
+                              id: 'timebox to update',
+                              text: 'updated string'
+                         }
+                    ]
+               }
+               );
+     })
+})
